9d31f9a88a8e18cabed63e73278ac200f5b95ccb
Catherine Newman <catherine.newman@solent.ac.uk>
2020-06-09 16:02:08 +0100

    adding init to js

--- amd/src/radiobuttons.js
+++ amd/src/radiobuttons.js
@@ -1,27 +1,25 @@
 define(['jquery'], function($) {
-    $("input[name='querytype']").click(function(){
-        var radioValue = $("input[name='querytype']:checked").val();
-        if(radioValue){
-            // return(radiovalue);
-            alert("Your are a - " + radioValue);
 
-            if(radioValue === 'Assessment_Missing_Dates_Incorrect') {
-                alert('yes');
-            } else if(radioValue === 'Assessment_Other') {
-                alert('No');
-            } else if(radioValue === 'Unit_leader_enrolment') {
+    return {
+        init: function() {
+            $("input[name='querytype']").click(function(){
+                var radioValue = $("input[name='querytype']:checked").val();
+                if(radioValue){
+                    // return(radiovalue);
+                    alert("Your are a - " + radioValue);
 
-            } else if (radioValue === 'Staff_other') {
-
-            } else if (radioValue === 'Access/account/password') {
-
-            } else if (radioValue === '') {
-                
-            }
+                    if(radioValue === 'Assessment_Missing_Dates_Incorrect') {
+                        alert('yes');
+                    } else if(radioValue === 'Assessment_Other') {
+                        alert('No');
+                    } else if(radioValue === 'Unit_leader_enrolment') {
+                    } else if (radioValue === 'Staff_other') {
+                    } else if (radioValue === 'Access/account/password') {
+                    }
 
 
-
-            return radioValue;
-       }
-    });
+               }
+            });
+        }
+    };
 });
